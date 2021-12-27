d0366c123931de793ab89fa5e99a623acf3041d1
xavioo <rucahoxavi@gmail.com>
2021-12-27 10:34:53 +0200

    add anchor on header links

--- web-antd-react/src/components/common/header.js
+++ web-antd-react/src/components/common/header.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Menu } from 'antd';
+import { Anchor } from 'antd';
+const { Link } = Anchor;
+
 function AppHeader() {
   return (
     <div className="container-fluid">
@@ -8,15 +10,16 @@ function AppHeader() {
           <i className="fas fa-desktop"></i>
           <a href="http://google.com">Ant-D-Web</a>
         </div>
-         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['home']}>
-        <Menu.Item key="home">Home</Menu.Item>
-        <Menu.Item key="about">About</Menu.Item>
-        <Menu.Item key="features">Features</Menu.Item>
-        <Menu.Item key="howitworks">How it works</Menu.Item>
-        <Menu.Item key="faq">FAQ</Menu.Item>
-        <Menu.Item key="pricing">Pricing</Menu.Item>
-        <Menu.Item key="contact">Contact</Menu.Item>
-      </Menu>
+          <Anchor targetOffset="65">
+            <Link href="#hero" title="Home" />
+            <Link href="#about" title="About" />
+            <Link href="#feature" title="Features" />
+            <Link href="#works" title="How it works" />
+            <Link href="#faq" title="FAQ" />
+            <Link href="#pricing" title="Pricing" />
+            <Link href="#contact" title="Contact" />
+          </Anchor>
+        
       </div>
     </div>
   );
