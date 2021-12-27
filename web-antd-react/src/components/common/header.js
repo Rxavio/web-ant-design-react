9f00af778f9cbc080c073d7e7853ec88e11fc73f
xavioo <rucahoxavi@gmail.com>
2021-12-27 11:00:20 +0200

    make it responsive

--- web-antd-react/src/components/common/header.js
+++ web-antd-react/src/components/common/header.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import { Anchor } from 'antd';
+import React, { useState } from 'react';
+
+import { Anchor, Drawer, Button } from 'antd';
+
 const { Link } = Anchor;
 
 function AppHeader() {
+  const [visible, setVisible] = useState(false);
+
+  const showDrawer = () => {
+    setVisible(true);
+  };
+
+  const onClose = () => {
+    setVisible(false);
+  };
+
   return (
     <div className="container-fluid">
       <div className="header">
         <div className="logo">
-          <i className="fas fa-desktop"></i>
-          <a href="http://google.com">Ant-D-Web</a>
+          <i className="fas fa-bolt"></i>
+          <a href="http://google.com">Tech</a>
         </div>
+        <div className="mobileHidden">
           <Anchor targetOffset="65">
             <Link href="#hero" title="Home" />
             <Link href="#about" title="About" />
@@ -19,7 +32,28 @@ function AppHeader() {
             <Link href="#pricing" title="Pricing" />
             <Link href="#contact" title="Contact" />
           </Anchor>
-        
+        </div>
+        <div className="mobileVisible">
+          <Button type="primary" onClick={showDrawer}>
+            <i className="fas fa-bars"></i>
+          </Button>
+          <Drawer
+            placement="left"
+            closable={false}
+            onClose={onClose}
+            visible={visible}
+          >
+            <Anchor targetOffset="65">
+              <Link href="#hero" title="Home" />
+              <Link href="#about" title="About" />
+              <Link href="#feature" title="Features" />
+              <Link href="#works" title="How it works" />
+              <Link href="#faq" title="FAQ" />
+              <Link href="#pricing" title="Pricing" />
+              <Link href="#contact" title="Contact" />
+            </Anchor>
+          </Drawer>
+        </div>
       </div>
     </div>
   );
